8eac26f821e3c68c60d8b63bc6fab649e1757f0a
4jamesccraven <4jamesccraven@gmail.com>
2025-01-30 03:01:21 -0500

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.mkdev = pkgs.callPackage ./package.nix { };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs.python312Packages; [
+            pkgs.python312
+            beautifulsoup4
+            requests
+            stanza
+            tqdm
+          ];
+
+          shellHook = ''
+            clear; zsh; exit
+          '';
+        };
+      }
+    );
+}
