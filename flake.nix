8c89da03af417b6d20e56b9fcadc4125ccca772d
4jamesccraven <4jamesccraven@gmail.com>
2025-03-20 15:23:01 -0400

    changing directions: - Previously it was intended to build a MT model, but that is not   feasible. Instead I will be focusing on building a parallel corpus   of biblical texts.

--- flake.nix
+++ flake.nix
@@ -14,32 +14,7 @@
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       in
       {
-        packages.mkdev = pkgs.callPackage ./package.nix { };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs.python312Packages; [
-            pkgs.python312
-            beautifulsoup4
-            numpy
-            requests
-            stanza
-            torch-bin
-            tqdm
-
-            pkgs.cudatoolkit
-            pkgs.linuxPackages.nvidia_x11
-          ];
-
-          shellHook = ''
-            clear
-            export CUDA_PATH=${pkgs.cudatoolkit}
-            export LD_LIBRARY_PATH=${pkgs.cudatoolkit}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:$LD_LIBRARY_PATH
-            export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-            export EXTRA_CCFLAGS="-I/usr/include"
-            zsh
-            exit
-          '';
-        };
+        
       }
     );
 }
